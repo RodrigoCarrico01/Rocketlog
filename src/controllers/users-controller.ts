deb450c37c6e1c0f25389d3b005431ebad853c2d
RodrigoCarrico01 <mr.rodrigocarrico@gmail.com>
2025-05-12 13:30:42 +0100

    User | validate data

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,18 @@
-import { Request, Response, NextFunction } from 'express';
+import e, { Request, Response, NextFunction } from 'express'
+import {z} from 'zod'
 
 class UsersController {
+
   create(request: Request, response: Response, next: NextFunction) {
-    return response.json({message: "User created successfully!"});
+    const bodySchema = z.object({
+      name: z.string().trim().min(3),
+      email: z.string().email(),
+      password: z.string().min(6),
+    })
+
+    const { name, email, password} = bodySchema.parse(request.body)
+    
+    return response.json({name, email, password})
   }
 }
 
