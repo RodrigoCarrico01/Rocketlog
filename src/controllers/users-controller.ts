29ca0667d0367d2f94696f3672e4098f518be73a
RodrigoCarrico01 <mr.rodrigocarrico@gmail.com>
2025-05-12 12:48:14 +0100

    User Route & Controller

---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express';
+
+class UsersController {
+  create(request: Request, response: Response, next: NextFunction) {
+    return response.json({message: "User created successfully!"});
+  }
+}
+
+export {UsersController}
